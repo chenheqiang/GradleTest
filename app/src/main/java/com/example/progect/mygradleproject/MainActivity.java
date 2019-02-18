bb1ee09128c483ff71f9f1d280a7fd859a6994c7
chenheqiang <chenheqiang@corp.netease.com>
2019-02-18 15:11:09 +0800

    commit

--- app/src/main/java/com/example/progect/mygradleproject/MainActivity.java
+++ app/src/main/java/com/example/progect/mygradleproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.progect.mygradleproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       String str =  BuildConfig.StrTest;
+        ((TextView) findViewById(R.id.test_tv)).setText(str);
     }
 }
